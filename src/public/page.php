c9c51c93cda08e748732ec0dc904a5f47a16e8cb
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-09 15:05:14 +0900

    fix:7-7新しい順、古い順での絞り込み機能

--- src/public/page.php
+++ src/public/page.php
@@ -15,9 +15,9 @@ $statement->execute();
 $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
+
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
