1802dc87ff268e3a8a3daf622402a607f0012560
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-09-10 19:56:13 +0900

    fix:11. 7-11絞り込み機能の練習⑥

--- src/public/mytop.php
+++ src/public/mytop.php
@@ -7,14 +7,35 @@ $pdo = new PDO(
     $dbPassword
 );
 
-$sql = 'SELECT * FROM pages';
+$search = $_GET['search'] ?? '';
+$start_date = $_GET['start_date'] ?? '';
+$end_date = $_GET['end_date'] ?? '';
+$order = $_GET['order'] ?? 'desc';
+
+if (!in_array($order, ['asc', 'desc'], true)) {
+  $order = 'desc';
+}
+
+$sql = "SELECT * FROM pages WHERE (name LIKE :search OR contents LIKE :search)";
+if (!empty($start_date)) {
+    $sql .= " AND created_at >= :start_date";
+}
+if (!empty($end_date)) {
+    $sql .= " AND created_at <= :end_date";
+}
+$sql .= " ORDER BY created_at $order";
+
 $statement = $pdo->prepare($sql);
-$statement->bindValue(':title', $title, PDO::PARAM_STR);
-$statement->bindValue(':content', $content, PDO::PARAM_STR);
+$statement->bindValue(':search', '%' . $search . '%', PDO::PARAM_STR);
+if (!empty($start_date)) {
+    $statement->bindValue(':start_date', $start_date, PDO::PARAM_STR);
+}
+if (!empty($end_date)) {
+    $statement->bindValue(':end_date', $end_date, PDO::PARAM_STR);
+}
 $statement->execute();
 $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
-
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -28,7 +49,10 @@ $pages = $statement->fetchAll(PDO::FETCH_ASSOC);
 <body>
   <div>
     <div>
-      <form action="index.php" method="get">
+      <form action="mytop.php" method="get">
+        <input type="text" name="search"><br>
+        <input type="date" name="start_date">
+        <input type="date" name="end_date">
         <div>
           <label>
             <input type="radio" name="order" value="desc" class="">
